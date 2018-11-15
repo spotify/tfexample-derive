4856cef5f5959a9ee570959dff7ee7eabb9a5e21
Andrew Martin <amartin@spotify.com>
2018-11-14 19:11:14 -0500

    Simplify derivation logic by propagating name prefix information through (#10)

--- src/main/scala/tf/magnolia/FeatureBuilder.scala
+++ src/main/scala/tf/magnolia/FeatureBuilder.scala
@@ -4,11 +4,10 @@ import org.tensorflow.example._
 import magnolia._
 
 import scala.language.experimental.macros
-import scala.collection.JavaConverters._
 
 trait FeatureBuilder[T] {
-  def toFeatures(record: T): Features.Builder
-  def fromFeatures(features: Features): T
+  def toFeatures(record: T, nameOrPrefix: Option[String]): Features.Builder
+  def fromFeatures(features: Features, nameOrPrefix: Option[String]): T
 }
 
 object FeatureBuilder {
@@ -16,36 +15,19 @@ object FeatureBuilder {
 
   def combine[T](caseClass: CaseClass[FeatureBuilder, T]): FeatureBuilder[T] =
     new FeatureBuilder[T] {
-      override def toFeatures(record: T): Features.Builder = {
-        val features = caseClass.parameters.map { p =>
-          composeWithName(p.label, p.typeclass)(p.dereference(record))
-        }
-        features.reduce { (fb1, fb2) =>
-          fb1.putAllFeature(fb2.getFeatureMap)
-          fb1
+      override def toFeatures(record: T, nameOrPrefix: Option[String]): Features.Builder = {
+        caseClass.parameters.foldLeft(Features.newBuilder()) { (fb, param) =>
+          val newPrefix = nameOrPrefix.fold(param.label)(prefix => s"$prefix.${param.label}")
+          fb.putAllFeature(param.typeclass.toFeatures(param.dereference(record), Some(newPrefix))
+            .getFeatureMap)
+          fb
         }
       }
 
-      override def fromFeatures(fs: Features): T = {
-        val fsMap = fs.getFeatureMap
-
+      override def fromFeatures(features: Features, nameOrPrefix: Option[String]): T = {
         caseClass.construct { param =>
-          val key = param.label
-          val fb = Features.newBuilder()
-          if (fsMap.containsKey(key)) {
-            // this is a primitive field
-            fb.putAllFeature(Map(key -> fsMap.get(key)).asJava)
-            param.typeclass.fromFeatures(fb.build())
-          } else {
-            // this is a nested field
-            // construct a new features map with keys starting with param.label
-            val keys = fsMap.keySet().asScala.filter(_.startsWith(key))
-            keys.foreach { key =>
-              val newKey = key.replaceFirst(".+\\.", "")
-              fb.putFeature(newKey, fsMap.get(key))
-            }
-            param.typeclass.fromFeatures(fb.build())
-          }
+          val newPrefix = nameOrPrefix.fold(param.label)(prefix => s"$prefix.${param.label}")
+          param.typeclass.fromFeatures(features, Some(newPrefix))
         }
       }
     }
@@ -53,28 +35,4 @@ object FeatureBuilder {
   def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): FeatureBuilder[T] = ???
 
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
-
-  private def composeWithName[T](name: String, instance: FeatureBuilder[T])
-  : T => Features.Builder = { record =>
-    val features = instance.toFeatures(record)
-    // If the parameter is -not- a nested case class, it should be treated as an anonymous
-    // (un-named) single feature. In this case, we name the feature using the param name.
-    // Instead of checking whether the param is a case class directly (via reflection),
-    // we can instead just check whether the generated features already have names or not.
-    // If the parameter -is- a nested case class, we prepend its feature names with this parameter
-    // name to prevent potential ambiguities.
-    if (features.getFeatureCount == 1 && features.containsFeature("")) {
-      val feature = features.getFeatureOrThrow("")
-      features.removeFeature("")
-      features.putFeature(name, feature)
-      features
-    }
-    else {
-      // TODO: do we really need scala conversion?
-      val fMap = features.getFeatureMap.asScala.map { case (fName, f) =>
-        (s"${name}.$fName", f)
-      }.asJava
-      Features.newBuilder().putAllFeature(fMap)
-    }
-  }
 }
