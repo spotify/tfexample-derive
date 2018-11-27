d63da7cd22d4e53ace645d485d4ad667cd1b33b1
Andrew Martin <andrewsmartin.mg@gmail.com>
2018-11-27 10:07:46 -0500

    Add scalafmt and scalastyle checks (#14)

--- build.sbt
+++ build.sbt
@@ -8,7 +8,6 @@ val tensorflowVersion = "1.7.0"
 
 val commonSettings = Seq(
   organization := "com.github.andrewsmartin",
-
   scalaVersion := "2.12.6",
   crossScalaVersions := Seq("2.11.12", "2.12.7"),
   scalacOptions ++= Seq("-target:jvm-1.8", "-deprecation", "-feature", "-unchecked"),
--- src/test/scala/tf/magnolia/ExampleConverterTest.scala
+++ src/test/scala/tf/magnolia/ExampleConverterTest.scala
@@ -33,14 +33,17 @@ class ExampleConverterTest extends FlatSpec with Matchers {
     val converter = ExampleConverter[BasicRecord]
     val record = BasicRecord(1, 2, 3.0f, ByteString.copyFromUtf8("hello"), "world")
     val actual = converter.toExample(record)
-    val expected = Example.newBuilder()
-        .setFeatures(Features.newBuilder()
+    val expected = Example
+      .newBuilder()
+      .setFeatures(
+        Features
+          .newBuilder()
           .putFeature("int", longFeat(1L))
           .putFeature("long", longFeat(2L))
           .putFeature("float", floatFeat(3.0f))
           .putFeature("bytes", stringFeat("hello"))
           .putFeature("string", stringFeat("world"))
-        .build)
+          .build)
     featuresOf(actual) shouldEqual featuresOf(expected)
     converter.fromExample(actual) shouldEqual record
   }
@@ -48,7 +51,7 @@ class ExampleConverterTest extends FlatSpec with Matchers {
   it should "support nested case class" in {
     case class Record(f1: Int, f2: Long, inner: Inner)
     case class Inner(f3: Long)
-    val record = Record(1, 2l, Inner(3l))
+    val record = Record(1, 2L, Inner(3L))
     val converter = ExampleConverter[Record]
     val example = converter.toExample(record)
     example.getFeatures.getFeatureCount shouldEqual 3
@@ -82,13 +85,16 @@ class ExampleConverterTest extends FlatSpec with Matchers {
     val converter = ExampleConverter[Record]
     val record = Record(1, List(1, 2, 3), Inner(Seq(1.0f, 2.0f), Array(true, false)))
     val example = converter.toExample(record)
-    val expected = Example.newBuilder()
-      .setFeatures(Features.newBuilder()
-        .putFeature("int", longFeat(1))
-        .putFeature("ints", longFeat(1L, 2L, 3L))
-        .putFeature("inner.floats", floatFeat(1.0f, 2.0f))
-        .putFeature("inner.bools", longFeat(1L, 0L))
-        .build)
+    val expected = Example
+      .newBuilder()
+      .setFeatures(
+        Features
+          .newBuilder()
+          .putFeature("int", longFeat(1))
+          .putFeature("ints", longFeat(1L, 2L, 3L))
+          .putFeature("inner.floats", floatFeat(1.0f, 2.0f))
+          .putFeature("inner.bools", longFeat(1L, 0L))
+          .build)
     featuresOf(example) shouldEqual featuresOf(expected)
     // Test round trip
     val newRecord = converter.fromExample(example)
@@ -102,20 +108,21 @@ class ExampleConverterTest extends FlatSpec with Matchers {
     class MyInt(val int: Int)
 
     implicit val myIntType: TensorflowMapping[MyInt] =
-      TensorflowMapping[MyInt](
-        f => toInts(f).map(n => new MyInt(n)),
-        myInts => fromInts(myInts.map(_.int)))
+      TensorflowMapping[MyInt](f => toInts(f).map(n => new MyInt(n)),
+                               myInts => fromInts(myInts.map(_.int)))
 
     case class Record(id: String, myInt: MyInt, myInts: List[MyInt])
     val converter = ExampleConverter[Record]
     val record = Record("1", new MyInt(1), List(new MyInt(2), new MyInt(3)))
     val example = converter.toExample(record)
-    val expected = Example.newBuilder()
-      .setFeatures(Features.newBuilder()
-        .putFeature("id", stringFeat("1"))
-        .putFeature("myInt", longFeat(1L))
-        .putFeature("myInts", longFeat(2L, 3L))
-      )
+    val expected = Example
+      .newBuilder()
+      .setFeatures(
+        Features
+          .newBuilder()
+          .putFeature("id", stringFeat("1"))
+          .putFeature("myInt", longFeat(1L))
+          .putFeature("myInts", longFeat(2L, 3L)))
       .build()
     featuresOf(example) shouldEqual featuresOf(expected)
     val newRecord = converter.fromExample(example)
@@ -131,12 +138,15 @@ class ExampleConverterTest extends FlatSpec with Matchers {
 
     // All Some()
     val record1 = OptionRecord(Some(2), Some(List(1.0f, 2.0f)), Some(Inner(true)))
-    val expected1 = Example.newBuilder()
-      .setFeatures(Features.newBuilder()
-        .putFeature("Some#int.value", longFeat(2L))
-        .putFeature("Some#floats.value", floatFeat(1.0f, 2.0f))
-        .putFeature("Some#inner.value.bool", longFeat(1L))
-        .build)
+    val expected1 = Example
+      .newBuilder()
+      .setFeatures(
+        Features
+          .newBuilder()
+          .putFeature("Some#int.value", longFeat(2L))
+          .putFeature("Some#floats.value", floatFeat(1.0f, 2.0f))
+          .putFeature("Some#inner.value.bool", longFeat(1L))
+          .build)
       .build()
     val example1 = converter.toExample(record1)
     // We can't assert directly due to Some.x vs Some.value difference between scala 2.11 / 2.12
@@ -149,7 +159,8 @@ class ExampleConverterTest extends FlatSpec with Matchers {
 
     // All None
     val record2 = OptionRecord(None, None, None)
-    val expected2 = Example.newBuilder()
+    val expected2 = Example
+      .newBuilder()
       .build()
     val example2 = converter.toExample(record2)
     featuresOf(example2) shouldEqual featuresOf(expected2)
@@ -171,7 +182,9 @@ class ExampleConverterTest extends FlatSpec with Matchers {
 
   private def stringFeat(str: String*): Feature = {
     val strings = str.map(ByteString.copyFromUtf8).asJava
-    Feature.newBuilder().setBytesList(BytesList.newBuilder().addAllValue(strings))
+    Feature
+      .newBuilder()
+      .setBytesList(BytesList.newBuilder().addAllValue(strings))
       .build()
   }
 
