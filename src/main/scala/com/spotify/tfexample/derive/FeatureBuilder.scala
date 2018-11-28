6480564245670af7aa4d4b3bf5edbf860bf5225c
Andrew Martin <amartin@spotify.com>
2018-11-28 14:47:19 -0500

    Use implicit lookup instead of dispatch for cleaner option type support

--- src/main/scala/com/spotify/tfexample/derive/FeatureBuilder.scala
+++ src/main/scala/com/spotify/tfexample/derive/FeatureBuilder.scala
@@ -17,8 +17,6 @@
 
 package com.spotify.tfexample.derive
 
-import java.util.function.Predicate
-
 import org.tensorflow.example._
 import magnolia._
 
@@ -51,36 +49,7 @@ object FeatureBuilder {
       }
     }
 
-  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): FeatureBuilder[T] =
-    new FeatureBuilder[T] {
-      override def toFeatures(record: T, nameOrPrefix: Option[String]): Features.Builder =
-        sealedTrait.dispatch(record) { subtype =>
-          val name = s"${subtype.typeName.short}"
-          val newPrefix = nameOrPrefix.fold(name)(n => s"$name#$n")
-          subtype.typeclass.toFeatures(subtype.cast(record), Some(newPrefix))
-        }
-
-      override def fromFeatures(features: Features, nameOrPrefix: Option[String]): T = {
-        // Note: this is hardcoded to only support Option for now, to minimize complexity. This
-        // may change in the future.
-        val subtypes = sealedTrait.subtypes.map(tpe => tpe.typeName.full -> tpe).toMap
-        val namePrefix = s"Some#${nameOrPrefix.getOrElse("")}"
-        if (subtypes.contains("scala.Some") && subtypes.contains("scala.None")) {
-          val keys = features.getFeatureMap.keySet()
-          if (keys
-                .stream()
-                .anyMatch(new Predicate[String] {
-                  override def test(t: String): Boolean = t.startsWith(namePrefix)
-                })) {
-            subtypes("scala.Some").typeclass.fromFeatures(features, Some(namePrefix))
-          } else {
-            None.asInstanceOf[T]
-          }
-        } else {
-          ???
-        }
-      }
-    }
+  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): FeatureBuilder[T] = ???
 
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 }
